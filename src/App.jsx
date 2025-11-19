37b1ebf27c5a3b7cffb2eb5817f9297e185ffa75
Hariom Thakur <HARIKRDBG121@GMAIL.COM>
2025-11-19 09:28:41 +0530

    Create App component with BirthdayPage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BirthdayPage from './components/BirthdayPage';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BirthdayPage friendName="Sarah" />
+    </div>
+  );
+}
+
+export default App;
